53a593945ffbb83d210e224f6095ad2833e45cad
aznovar <kkrieger5@gmail.com>
2019-10-27 22:54:38 +0300

    Add fixed texture in settings screen

--- core/src/com/mygdx/game/gamescreens/SettingsScreen.java
+++ core/src/com/mygdx/game/gamescreens/SettingsScreen.java
@@ -3,14 +3,10 @@ package com.mygdx.game.gamescreens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.mygdx.game.MyGdxGame;
-import com.mygdx.game.assets.gui.SettingsUIFragment;
 
 public class SettingsScreen extends ScreenAdapter {
 
@@ -29,7 +25,7 @@ public class SettingsScreen extends ScreenAdapter {
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(textureRegion,64.0f,64.0f,textureRegion.getRegionWidth(),textureRegion.getRegionHeight());
+        batch.draw(textureRegion,768.0f/3f,70.0f,1660,1010);
         batch.end();
     }
 
